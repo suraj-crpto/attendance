3c855c844a51992b6970ab9695fa33b916c0fae8
Suraj Singh <surajsingj597@gmail.com>
2022-11-29 08:28:10 +0530

    changes for git

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/navbar';
-
+import Container from './Components/Container';
+import Footer from './Components/footer';
 function App() {
   return (
     <>
     <Navbar/>
-    </>  );
+    <Container/>
+    <Footer/>
+    </>
+      );
 }
-
 export default App;
---
+++ src/Components/Container.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class container extends React.Component {
+    render() {
+        return (
+            <div class="container">
+                <h1>My First Bootstrap Page</h1>
+                <p>This is some text.</p>
+            </div>
+        );
+    }
+}
+export default container
